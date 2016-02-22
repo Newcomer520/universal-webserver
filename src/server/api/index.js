59fe05fed81a5d3ece70cff2d8630aefb231b006
I-Lun Chen <ilun_chen@wistron.com>
2016-02-22 09:28:37 +0800

    fix routes matching issue, should cb() even if redirect to login

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,16 +1,12 @@
 import { Router } from 'express'
 import loginRouter from './login'
 import logoutRouter from './logout'
-import dummyRouter from './dummy-data'
-import aDataRouter from './authorized-data'
 import refreshTokenRouter from './refresh-token'
 import statusRouter from './status'
 
 const apiRouter = new Router()
 apiRouter.use('/login', loginRouter)
 apiRouter.use('/logout', logoutRouter)
-apiRouter.use('/dummy', dummyRouter)
-apiRouter.use('/adata', aDataRouter)
 apiRouter.use('/refreshtoken', refreshTokenRouter)
 apiRouter.use('/status', statusRouter)
 apiRouter.use((err, req, res, next) => {
