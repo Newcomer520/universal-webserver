3adecca0c1c1991589ced9825ab5d9269bca286d
I-Lun Chen <ilun_chen@wistron.com>
2016-03-09 21:58:01 +0800

    use the co (generator) way to initialize redis

--- src/server/utils/redis.js
+++ src/server/utils/redis.js
@@ -7,16 +7,24 @@ const { redisIp, redisPort } = global.config
 const log = type => () => console.log(type, arguments)
 
 
-const client = redis.createClient({ host: redisIp, port: redisPort, max_attempts: 10 })
+var client
 
 // client.on('connect', log('connect'))
 // client.on('ready', log('ready'))
 // client.on('reconnecting', log('reconnecting'))
-client.on('error', () => console.log(`failed to connect redis server: ${redisIp}:${redisPort}`))
-client.on('end', () => console.log('redis end'))
-process.on('exit', code => {
-	console.log('redis exit....')
-	client.end()
-})
 
 export default client
+
+export const initRedis = () => (done) => {
+	client = redis.createClient({ host: redisIp, port: redisPort, max_attempts: 10 })
+	client.on('ready', () => {
+		console.info('redis connected.')
+		done()
+	})
+	client.on('error', () => console.log(`failed to connect redis server: ${redisIp}:${redisPort}`))
+	client.on('end', () => console.log('redis end'))
+	process.on('exit', code => {
+		console.log('redis exit....')
+		client.end()
+	})
+}
