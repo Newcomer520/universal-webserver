54523b5c5520b71c12845805f2a77d42a726a86a
I-Lun Chen <ilun_chen@wistron.com>
2016-03-25 09:34:24 +0800

    remove the dependencies of Radium

--- src/app/containers/NoMatch/NoMatch.js
+++ src/app/containers/NoMatch/NoMatch.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
-import Radium from 'radium'
 
-@Radium
 export default class extends Component {
 	state = {
 		countDown: 5
@@ -22,22 +20,21 @@ export default class extends Component {
 		this.timer && clearInterval(this.timer)
 	}
 	render() {
+		const style = {
+			marginLeft: 'auto',
+			marginRight: 'auto',
+			height: '100vh',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			textAlign: 'center'
+		}
+
 		return (
-			<div style={styles.base}>
+			<div style={style}>
 				<h2>Cannt goto "{this.props.location.pathname}", redirect to the default page in {this.state.countDown} seconds.</h2>
 			</div>
 		)
 	}
 }
 
-const styles = {
-	base: {
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		height: '100vh',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		textAlign: 'center'
-	}
-}
